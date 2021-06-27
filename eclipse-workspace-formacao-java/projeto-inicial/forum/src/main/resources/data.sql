873fe5d4b7af9d7217f262daeb46a255522d9167
andreamboni <amboni.andre@gmail.com>
2021-06-27 19:41:22 -0300

    Mais algumas aulas de API Rest

--- eclipse-workspace-formacao-java/projeto-inicial/forum/src/main/resources/data.sql
+++ eclipse-workspace-formacao-java/projeto-inicial/forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$OExXrV8dZpxKJ3cNCyoDN.84h4BxL8L8CyLGfB2YUgq6l90K3pZNe');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
