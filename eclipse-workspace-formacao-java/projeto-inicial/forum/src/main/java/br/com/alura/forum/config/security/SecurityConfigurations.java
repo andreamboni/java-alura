cd4871f83d1c9d527a5bb70afc28e25d4af70e3a
Andre Amboni <amboni.andre@gmail.com>
2021-07-07 21:43:46 -0300

    Começando terceiro e ultimo curso de Spring Boot

--- eclipse-workspace-formacao-java/projeto-inicial/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ eclipse-workspace-formacao-java/projeto-inicial/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -44,12 +44,16 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	// Configuração de permissão
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
-				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll().antMatchers(HttpMethod.POST, "/auth").permitAll()
-				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll().anyRequest().authenticated().and().csrf()
-				.disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				.addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository),
-						UsernamePasswordAuthenticationFilter.class);
+		http.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+				.antMatchers(HttpMethod.POST, "/auth").permitAll()
+				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+				.antMatchers(HttpMethod.DELETE, "/actuator/**").hasRole("MODERADOR")
+				.anyRequest().authenticated()
+				.and().csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository),	UsernamePasswordAuthenticationFilter.class);
 	}
 
 	// Configuracao de recursos estaticos (js, css, imgs e etc.)
